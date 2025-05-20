5c29a7e709cfbe92c1fc868434abe398189c3549
Abass Ibrahim <profbass4all@gmail.com>
2025-05-20 05:13:08 +0100

    solved cors for production

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = 3000;
 
 app.use((req, res, next) => {
     
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173')
+    res.header('Access-Control-Allow-Origin', 'https://3mtt-movie-recommendation.netlify.app')
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
     res.header('Access-Control-Expose-Headers', 'Authorization')
